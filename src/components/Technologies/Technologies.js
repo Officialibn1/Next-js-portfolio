6d92f1cc1a6b405daa010afb355e43df4af4360b
Officialibn1 <officialibn001@gmail.com>
2023-04-30 11:20:42 +0100

    Technologies section complete

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,9 +4,62 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id='tech'>
+    <SectionDivider />
+
+    <SectionTitle>Technologies</SectionTitle>
+
+    <SectionText>
+      I've worked with a range of web develpement technologies, frameworks and libraries to build appealing website for my clients.
+    </SectionText>
+
+    <List>
+
+      <ListItem>
+        <DiReact size='5rem'/>
+
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+
+          <ListParagraph>
+            Experience with <br />
+            React Js <br />
+            Next Js <br />
+            Styled-Components
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+
+      <ListItem>
+        <DiFirebase size='5rem'/>
+
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+
+          <ListParagraph>
+            Experience with <br />
+            Node Js <br />
+            and some <br />
+            porpular DataBases
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+
+      <ListItem>
+        <DiZend size='5rem'/>
+
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+
+          <ListParagraph>
+            Experience with <br />
+            tools like Figma
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+
+    </List>
+  </Section>
 );
 
 export default Technologies;
