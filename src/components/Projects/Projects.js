7d26112109d3df20b188fe4de7bf2785034ddeb6
Officialibn1 <officialibn001@gmail.com>
2023-04-30 10:52:07 +0100

    Projects section complete

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,9 +5,58 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { projects } from '../../constants/constants';
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id='projects'>
+
+    <SectionDivider />
+
+    <SectionTitle main>Projects</SectionTitle>
+
+    <GridContainer>
+
+      { projects.map(({ title, description, image, tags, source, visit}, index) => (
+        <BlogCard key={index}>
+
+          <Img src={image} />
+
+          <TitleContent>
+            <HeaderThree title>
+              {title}
+            </HeaderThree>
+
+            <Hr />
+          </TitleContent>
+
+          <CardInfo>
+            {description}
+          </CardInfo>
+
+          <div style={{marginTop: '25px'}}>
+            <TitleContent>Stack</TitleContent>
+
+            <TagList>
+
+              {tags.map((tag, i) => (
+                <Tag key={i}>
+                  {tag}
+                </Tag>
+              ))}
+
+            </TagList>
+          </div>
+
+          <UtilityList>
+
+            <ExternalLinks href={visit}>Code</ExternalLinks>
+
+            <ExternalLinks href={source}>Source</ExternalLinks>
+            
+          </UtilityList>
+
+        </BlogCard>
+      ))}
+
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
