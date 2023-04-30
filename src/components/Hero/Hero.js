33a2574583874fcf9b23a0f8ac27b2a782676791
Officialibn1 <officialibn001@gmail.com>
2023-04-30 09:52:33 +0100

    Hero section complete

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,26 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    
+    <LeftSection>
+
+      <SectionTitle main center>
+        Welcome To <br />
+        My Portfolio
+      </SectionTitle>
+
+      <SectionText>
+        Hello Dear, I'm ibn a Web Developer who have helped lot's of business owners and companies create a stunning online representation of their company and services with websites. <br /> It'll be my pleasure to bring your dream website to reaity. Feel free to contact me for any jobs, I'll be available for your needs.
+      </SectionText>
+
+      <Button>
+        Hire Me
+      </Button>
+      
+    </LeftSection>
+
+  </Section>
 );
 
 export default Hero;
