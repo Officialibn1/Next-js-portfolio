b8766add99c223b3fd8449092ee14bf9d1abe108
Officialibn1 <officialibn001@gmail.com>
2023-05-01 12:49:33 +0100

    Timeline Section Complete

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -6,7 +6,7 @@ import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './Techn
 const Technologies = () =>  (
   <Section id='tech'>
     <SectionDivider />
-
+    <br /><br />
     <SectionTitle>Technologies</SectionTitle>
 
     <SectionText>
